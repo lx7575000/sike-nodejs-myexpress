282a769c877916e3684072129d21ebb79ee479da
lx7575000 <liu_narcisus@163.com>
2015-05-16 16:07:11 +0800

    step2
    第二个测试用例，写完了。发现自己其实对mocha和chai，supertest其实了解的很少。

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+var http = require('http');
+
+
 module.exports = myexpress;
 function myexpress(){
-	return function(req, res, next){
+	var app = function(req, res, next){
 		res.statusCode = 404;
 		res.end();
 	};
+
+	app.listen = function(port){
+		var server = http.createServer(app);
+		server.listen(port);
+		return server;
+	}
+
+	return app;
 };
--- test/app_spec.js
+++ test/app_spec.js
@@ -5,6 +5,24 @@ var http = require('http');
 
 describe('Implement Empty App', function(){
 	describe('as handler to http.createServer', function(){
+		it('create a server should return 404', function(done){
+			var app = myexpress();
+			app.listen(5001);
+
+			request('http://localhost:5001')
+					.get('/foo')
+					.expect(404)
+					.end(done());
+
+		});
+
+		it('should return an http.Server', function(done){
+			var app = myexpress();
+			var server = app.listen(4001);
+
+			expect(server).to.be.instanceof(http.Server);
+			done();
+		});
 
 		it('responds to /foo with 404', function(done){
 			var app = myexpress();
